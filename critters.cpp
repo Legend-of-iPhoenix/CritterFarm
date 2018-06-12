4f6c3304db4b4b162008d950949a67a79a311720
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-06-12 14:01:27 -0400

    Update code.

--- critters.cpp
+++ critters.cpp
@@ -97,10 +97,67 @@ class Player {
   void tick() {
     for (int i = 0; i < 10; i++) {
       gold += (*critters[i]).getEarningsRate();
-      if (rand() % 10 == 1) {
+      if (rand() % 10 == 0) {
         (*critters[i]).damage(1 + (rand() % 5));
       }
     }
+  }
+
+  bool buyCritter(int cost, int goldRate, int maxHealth) {
+  	if (cost <= gold) {
+  		bool found = false;
+  		for (int i = 0; i < 10; i++) {
+  			if ((*critters[i]).maxHealth == 0 && !found) {
+  				found = true;
+  				delete critters[i];
+  				critters[i] = new Critter(maxHealth, goldRate);
+  				gold -= cost;
+  				std::cout << "Successfully purchased a critter for " << cost << " gold!" << std::endl;
+  				return true;
+  			}
+  		}
+  		if (!found) {
+  			std::cout << "Your critter slots are full!" << std::endl;
+  			return false;
+  		}
+  	} else {
+  		std::cout << "Purchase failed: Insufficient funds." << std::endl;
+  		return false;
+  	}
+  }
+
+  bool buyCritterFood(int cost, int level, int quantity) {
+  	if (cost <= gold) {
+  		bool found = false;
+  		for (int i = 0; i < 10; i++) {
+  			if ((*inventory[i]).level == level && !found) {
+  				found = true;
+  				CritterFood critterFood = (*inventory[i]);
+  				delete inventory[i];
+  				inventory[i] = new CritterFood(level, critterFood.stackSize + quantity);
+  				gold -= cost;
+  				std::cout << "Successfully purchased " << quantity << " Level " << level << " critter food for " << cost << " gold!" << std::endl;
+  				return true;
+  			}
+  		}
+  		if (!found) {
+  			for (int i = 0; i < 10; i++) {
+	  			if ((*inventory[i]).level == 0 && !found) {
+	  				found = true;
+	  				delete inventory[i];
+	  				inventory[i] = new CritterFood(level, quantity);
+	  				gold -= cost;
+	  				std::cout << "Successfully purchased " << quantity << " Level " << level << " critter food for " << cost << " gold!" << std::endl;
+	  				return true;
+	  			}
+	  		}
+  			std::cout << "Your inventory is full!" << std::endl;
+  			return false;
+  		}
+  	} else {
+  		std::cout << "Purchase failed: Insufficient funds." << std::endl;
+  		return false;
+  	}
   }
 
   Player();
@@ -116,37 +173,71 @@ Player::Player() {
 
 class Shop {
  public:
-  int costs[10], quantities[10], levels[10], types[10];
-  void show() {
-    for (int i = 0; i < 10; i++) {
-      if (rand() % 2 == 1) {
-        levels[i] = 1 + (rand() % 10);
-        quantities[i] = 1 + (rand() % 20);
-        costs[i] = levels[i] * quantities[i] - (rand() % quantities[i]);
-        if (costs[i] <= 0) {
-          costs[i] = 1;
-        }
-        std::cout << " " << i << ": Level " << levels[i] << " Critter Food (*"
-                  << quantities[i] << ")     Cost: " << costs[i] << " gold. "
-                  << std::endl;
-      } else {
-        levels[i] = 1 + (rand() % 10);
-        quantities[i] = 10 * (rand() % 20);
-        costs[i] = levels[i] * levels[i] * quantities[i] - (rand() % 100);
-        if (costs[i] <= 0) {
-          costs[i] = 1;
-        }
-        std::cout << " " << i << ": Critter (" << levels[i]
-                  << " gold per tick, has max health of " << quantities[i]
-                  << ")     Cost: " << costs[i] << " gold. " << std::endl;
-      }
-    }
+  int costs[10], quantities[10], levels[10], types[10], bought[10];
+  bool set;
+  void show(Player player) {
+  	std::string option;
+  	do {
+	  	std::cout << "You have " << player.gold << " gold." << std::endl;
+
+	    for (int i = 0; i < 10; i++) {
+	      if (i <= 5) {
+	      	if (!set) {
+		        levels[i] = 1 + (rand() % 10);
+		        quantities[i] = 1 + (rand() % 20);
+		        costs[i] = levels[i] * quantities[i];
+		        if (costs[i] <= 0) {
+		          costs[i] = 1;
+		        }
+	      	}
+	        std::cout << " " << i << ": Level " << levels[i] << " Critter Food (*"
+	                  << quantities[i] << ")     Cost: " << costs[i] << " gold. "
+	                  << std::endl;
+	      } else {
+	      	if (!set) {
+		        levels[i] = 2 + (rand() % 10);
+		        quantities[i] = 10 * (rand() % 20);
+		        costs[i] = levels[i] * levels[i] * quantities[i] - (rand() % 100);
+		        if (costs[i] <= 0) {
+		          costs[i] = 1;
+		        }
+	      	}
+	        std::cout << " " << i << ": Critter (" << levels[i]
+	                  << " gold per tick, has max health of " << quantities[i]
+	                  << ")     Cost: " << costs[i] << " gold. " << std::endl;
+	      }
+	    }
+	    std::cout << "10: Exit" << std::endl << std::endl;
+	    std::cout << " Select an option (0-10): ";
+	    set = true;
+	   	getline(std::cin, option);
+	   	if (option.length() == 1) {
+	   		int selection = int(option.at(0));
+	   		if (selection >= 48 && selection <= 53) {
+	   			if (!bought[selection-48]) {
+	   				if (player.buyCritterFood(costs[selection - 48], levels[selection - 48], quantities[selection - 48])) {
+	   					bought[selection-48] = true;
+	   				}
+	   			}
+	   		} else {
+	   			if (selection >= 54 && selection <= 57) {
+	   				if (!bought[selection-48]) {
+	   					if (player.buyCritter(costs[selection - 48], levels[selection - 48], quantities[selection - 48])) {
+	   						bought[selection-48] = true;
+	   					}
+	   				}
+	   			}
+	   		}
+	   	}
+  	}
+    while (option != "10");
   }
 };
 
 int main() {
   Player player;
-  delete player.critters[0]; // before setting an inventory slot or otherwise, delete whatever junk was in there before
+  player.gold = 100;
+  delete player.critters[0]; // before setting an inventory slot or otherwise, delete whatever junk was in there before (prevents memory leaks)
   player.critters[0] = new Critter(100, 5);
   (*player.critters[0]).damage(10);
   (*player.critters[0]).introduce();
@@ -161,6 +252,7 @@ int main() {
   player.displayInventory();
   (*player.critters[0]).introduce();
   Shop shop;
-  shop.show();
+  shop.show(player);
+  player.displayInventory();
   return 0;
 }
